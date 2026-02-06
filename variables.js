2047ce3a1185fbd4eda4b8db8571ac6bdb83ab90
AnnCodin <qantoinnette23@gmail.com>
2026-02-06 17:46:13 +0000

    Change the value of favFood and add a new variable named my birthday

--- variables.js
+++ variables.js
@@ -3,3 +3,12 @@ let favColor = "Red";
 let favDrink = "Lemonade";
 
 console.log(favDrink);
+console.log(favFood);
+
+favFood = "Pasta";
+
+const myBirthday = "6th February"
+
+console.log(myBirthday);
+
+
