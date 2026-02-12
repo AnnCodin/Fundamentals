feccb14b39ef380c386661fcf4373697485c53d3
AnnCodin <qantoinnette23@gmail.com>
2026-02-12 21:48:28 +0000

    Add number methods

--- numbers.js
+++ numbers.js
@@ -12,3 +12,13 @@ console.log(num2.toPrecision(3));
 
 let lightSpeed = 186000;
 console.log(lightSpeed.toExponential());
+
+//Number Methods
+const pi = 3.141562542;
+const num3 = pi.toFixed(2);
+let num3 = pi.toPrecision(5);
+console.log(num3);
+
+let mill = 1000000;
+num = mill.toExponential();
+console.log(num);
