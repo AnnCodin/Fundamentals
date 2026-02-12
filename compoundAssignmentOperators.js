306aee87f33b95c7adf21049ca09e70d0688b865
Antoinnette Quarshie <qantoinnette23@gmail.com>
2026-02-12 21:44:53 +0000

    Add files via upload

---
+++ compoundAssignmentOperators.js
@@ -0,0 +1,24 @@
+// += Operator
+let score = 100;
+score += 100;
+console.log(score);
+
+//-+ Operator
+let balance = 4000;
+balance -= 400;
+console.log(balance);
+
+//*= Operator
+let donation = 100;
+donation *= 2;
+console.log(donation);
+
+// /= Operator
+let sweets = 10;
+sweets /= 10;
+console.log(sweets);
+
+// %= Operator
+let pizzaSlices = 10;
+pizzaSlices %= 3;
+console.log(pizzaSlices);
