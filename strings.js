932c74079846b6af0a1787b87f366375bb651170
AnnCodin <qantoinnette23@gmail.com>
2026-02-12 21:55:23 +0000

    Add speech marks

---
+++ strings.js
@@ -0,0 +1,11 @@
+//Single, double, backtick
+let s = "single";
+let d = "double";
+let b = `backtick`;
+
+//Speech Marks
+let speech = 'Martin Luther King said: "I have a dream."';
+console.log(speech);
+
+let tip = "We can escape quotes by using \\";
+console.log(tip);
