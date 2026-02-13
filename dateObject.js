0e856bd212d5f649d1be38a528688cd136528b4b
AnnCodin <qantoinnette23@gmail.com>
2026-02-13 20:11:59 +0000

    Log the name of the current day using long hand

--- dateObject.js
+++ dateObject.js
@@ -4,3 +4,6 @@ console.log(date.getDate());
 let hour = date.getHours();
 let min = date.getMinutes();
 console.log(`${hour}:${min}`);
+
+let day = date.toLocaleString("default", { weekday: "long" });
+console.log(day);
