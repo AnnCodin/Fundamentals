41513bb28d66da622b0cda5f284bbbf9dad7f746
AnnCodin <qantoinnette23@gmail.com>
2026-03-11 14:07:11 +0000

    Add a multidimensional array of colours and access the last colour in the second level

--- arrays.js
+++ arrays.js
@@ -3,3 +3,14 @@ let countries = ["Ghana", "Nigeria", "Benin"];
 countries[3] = "Togo";
 
 console.log(countries);
+
+//Multi-dimensional arrays
+
+let colours = [
+  "mid-night blue",
+  "indigo",
+  "purple",
+  ["black", "white", "grey"],
+];
+
+console.log(colours[3][2]);
